b29563eab202a0acbf9ac9930db716f84afada83
JMEPJ\PC <1485165003@qq.com>
2018-04-11 13:21:38 +0800

    利用express的get和post实现对不同URL路径访问的处理，入门

--- app.js
+++ app.js
@@ -8,5 +8,14 @@
  var app = express();
 
 
+ //根据访问的类型和访问的URL路径，分类进行处理
+ app.get('/',function(req,res,next){
+     console.log(res)
+     res.send("欢迎访问我的博客");
+ })
+
+
  //监听http请求
  app.listen(8081);
+
+ //http://localhost:8081/
