d01246cdb2139c49b3bcde011c30f6e37ea4220c
JMEPJ\PC <1485165003@qq.com>
2018-04-13 13:45:18 +0800

    利用数据库limit,skip实现分页显示

--- routers/admin.js
+++ routers/admin.js
@@ -9,26 +9,53 @@ var router = express.Router();
 /**
  * 首页
  */
-router.get('/',function(req,res,next){
-    
-    res.render("admin/index",{
-        userInfo:req.userInfo
+router.get('/', function (req, res, next) {
+
+    res.render("admin/index", {
+        userInfo: req.userInfo
     })
-  
+
 })
 
 /**
  * 用户管理
  */
-router.get('/user',function(req,res,next){
-   //从数据库中读取所有的用户数据
-    User.find().then(function(users){
-        // console.log(users);
-        res.render("admin/user_index",{
-            userInfo:req.userInfo,
-            users:users
+router.get('/user', function (req, res, next) {
+    /**
+     * 从数据库中读取所有的用户数据
+     * 分页：
+     * 每页显示两条,利用URL的？查询条目实现分页
+     * 1，一页限制两条,limit(Number)
+     * 2,skip（），进行跳跃
+     */
+    var page = Number(req.query.page || 1);
+    var limit = 2;
+    var pages = 0;
+
+    User.count().then(function(count){
+        
+        //计算总页数
+        pages = Math.ceil(count / limit);
+        //page不能超过总页数 pages
+        page = Math.min(page,pages)
+        //取值不能小于1
+        page = Math.max(page,1);
+        var skip = (page-1) * limit;
+
+        User.find().limit(limit).skip(skip).then(function (users) {
+            // console.log(users);
+            res.render("admin/user_index", {
+                userInfo: req.userInfo,
+                users: users,
+    
+                page:page,
+                count:count,
+                limit:limit,
+                pages:pages,
+            })
         })
     })
+    
 })
 
 module.exports = router;
