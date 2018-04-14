059fcb01adcb52c1ac0e11949143d26384d20ff5
JMEPJ\PC <1485165003@qq.com>
2018-04-14 15:22:00 +0800

    博客文章阅读全文页面显示

--- routers/main.js
+++ routers/main.js
@@ -9,32 +9,50 @@ let Content = require('../models/Content')
 //用来监听URL为/main开头的请求
 var router = express.Router();
 
+
+var data;
+/**
+ * 中间件处理通用数据
+ */
+router.use(function (req, res, next) {
+    data = {
+        userInfo: req.userInfo,
+        categories: [],
+    }
+    Category.find().then(function (categories) {
+        data.categories = categories;
+        next();
+    })
+})
 /**
  * 首页展示
  */
 router.get('/', function (req, res, next) {
-    console.log(req.userInfo)
-    var data = {
-        page: Number(req.query.page || 1),
-        limit: 2,
-        pages: 0,
-        count: 0,
-        userInfo: req.userInfo,
-        category:req.query.category || '',
-        categories: [],
-        contents: [],
-    }
+    // console.log(req.userInfo)
+    // data = {
+    //     page: Number(req.query.page || 1),
+    //     limit: 2,
+    //     count: 0,
+    //     category: req.query.category || '',
+    //     contents: [],
+    // }
+    data.page = Number(req.query.page || 1);
+    data.limit = 2;
+    data.count = 0;
+    data.category = req.query.category || '';
+    data.pages = 0;
 
     var where = {};
-    if(data.category){
+    if (data.category) {
         where.category = data.category
     }
 
     //读取分类信息
-    Category.find().then(function (categories) {
-        data.categories = categories;
-        return Content.where(where).count();
-    })
+    // Category.find().then(function (categories) {
+
+    //     return Content.where(where).count();
+    // })
+    Content.where(where).count()
         .then(function (count) {
             data.count = count;
             //计算总页数
@@ -46,7 +64,7 @@ router.get('/', function (req, res, next) {
             let skip = (data.page - 1) * data.limit;
 
             return Content.where(where).find().limit(data.limit).skip(skip).populate(['category', 'user'])
-            .sort({addTime:-1});
+                .sort({ addTime: -1 });
         })
         .then(function (contents) {
             data.contents = contents;
@@ -55,5 +73,19 @@ router.get('/', function (req, res, next) {
 
 })
 
+
+/**
+ * 阅读全文
+ */
+router.get('/view', function (req, res) {
+    var contentId = req.query.contentid || '';
+    Content.findOne({
+        _id: contentId
+    }).then(function (contents) {
+        data.contents = contents;
+
+        res.render('main/view',data);
+    })
+})
 
 module.exports = router;
